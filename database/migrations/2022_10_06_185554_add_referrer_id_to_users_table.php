27a944b300fcea7afae548703edea9036cf9e380
Joaquín Triñanes <joaquin@veganhacktivists.org>
2024-10-01 16:51:41 +0200

    Fix migration rollback

--- database/migrations/2022_10_06_185554_add_referrer_id_to_users_table.php
+++ database/migrations/2022_10_06_185554_add_referrer_id_to_users_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      *
@@ -28,7 +29,7 @@ return new class extends Migration {
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('referrer_id');
+            $table->dropConstrainedForeignId('referrer_id');
         });
     }
 };
